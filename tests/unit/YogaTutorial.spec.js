03855270e321e61f075ad7dccefd5a8c69d0cfe2
Alexander Zito-Wolf <azitowolf@Alexanders-MacBook-Pro-2.local>
2020-07-17 23:52:32 -0400

    Expanded tests on Tutorial component

--- tests/unit/YogaTutorial.spec.js
+++ tests/unit/YogaTutorial.spec.js
@@ -4,63 +4,40 @@ import YogaTutorial from "./../../src/components/YogaTutorial.vue";
 
 // Test Data
 
-const testProps = [
-  {
-    Description:
-      "Half moon pose, or ardha chandrasana, is both a standing and balancing posture. With one foot rooted firmly to the ground, the other lifts toward the sky. The hands do the same; one hand rests on the ground while the other lifts toward the sky. The pose develops focus and concentration.",
-    _createdAt: "2020-06-27T15:08:27Z",
-    _id: "067e6398-855e-4c64-82ba-9e9638bbd2d9",
-    _rev: "fC2enKhGoDy6Fwhgs4Pova",
-    _type: "pose",
-    _updatedAt: "2020-07-05T15:12:26Z",
-    aliases: ["half moon", "hfmn"],
-    difficulty: {
-      _ref: "2b11d4de-c6b9-4f04-9fcc-334031e1fc8c",
-      _type: "reference",
-    },
-    image: {
-      _type: "image",
-      asset: {
-        _ref: "image-4a20044b0501c8c918854a0dfed09b8902b873b7-130x130-png",
-        _type: "reference",
-      },
-    },
-    name: "Half Moon",
-    sanskrit: "Ardha Chandrasana",
-  },
-];
+import TestData from "./../TestData.json";
 
 // Mount the component
 const wrapper = shallowMount(YogaTutorial);
-console.log(wrapper.vm);
+const deeperWrapper = shallowMount(YogaTutorial, {
+  propsData: {
+    poses: TestData.result
+  }
+});
 
 describe("YogaTutorial", () => {
-  // Inspect the raw component options
-  it("has a created hook", () => {
-    expect(typeof YogaTutorial.created).toBe("function");
+
+  // testing test setup successful
+  test("is a Vue instance", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(deeperWrapper.isVueInstance()).toBeTruthy();
   });
 
-  // Mount an instance and inspect the render output
-  it("renders the wrapper", () => {
-    expect(typeof wrapper.element).toBe("object");
-    expect(wrapper.element.classList).toContain("yoga-tutorial-component");
+  it("renders the correct markup", () => {
+    expect(wrapper.html()).toContain(
+      "<p>The idea is simple: You enter your sequence in the format below:</p>"
+    );
   });
 
-  // Mount an instance with props
-  it("renders the tutorial list", () => {
-    expect(wrapper.exists()).toBe(true);
-    expect(wrapper.find(".tutorial-list").exists()).toBe(true);
-    // renders a single yoga pose with the name correctly    });
+  it("has convert button", () => {
+    expect(wrapper.contains(".supported-poses-list")).toBe(true);
   });
 
-  it("renders as many poses as are passed", () => {
-    const wrapperWithProps = mount(YogaTutorial);
-    wrapperWithProps.setProps({ poses: testProps });
-
-    expect(wrapperWithProps.vm.poses).toBe(testProps);
-
-    const $aliasesArray = wrapperWithProps.findAll("pre");
-    console.log($aliasesArray);
-    expect($aliasesArray.length).toBe(2);
+  it("contains the expected props", () => {
+    expect(deeperWrapper.vm.poses).toBe(TestData.result);
   });
+
+  // it("renders all poses", () => {
+  //   expect(document.getElementsByClassName('.alias-dropdown')).toHaveLength(TestData.length);
+  // });
+
 });
